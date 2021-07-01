4783f7dc906e38b12af1f22b80f3171e20bc6ace
tin0819tin <antony19990819@gmail.com>
2021-07-02 02:47:33 +0800

    Adding Tutor html and js

---
+++ frontend/js/Tutor.js
@@ -0,0 +1,7 @@
+// Initialization
+let DS = []
+const OM = new ObjectManager();
+const AM = new AnimationManager(OM, document, DS[0]);
+AM.DIYMode = false;
+
+// Random the DS question and check the answer
